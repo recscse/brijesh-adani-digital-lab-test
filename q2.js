216d77644cb845e0dc8b806fcc9587ccee8d7db7
recscse <recs.cse17903@gmail.com>
2021-09-24 15:20:13 +0530

    comit

---
+++ q2.js
@@ -0,0 +1,159 @@
+const awards = [
+
+    {
+
+    name: 'James Peebles',
+
+    category: 'Physics',
+
+    research: 'Theoretical discoveries in physical cosmology',
+
+    year: 2019,
+
+  },
+
+  {
+
+    name: 'Michel Mayor',
+
+    category: 'Physics',
+
+    research: 'Discovery of an exoplanet orbiting a solar-type star',
+
+    year: 2019,
+
+  },
+
+  {
+
+    name: 'Didier Queloz',
+
+    category: 'Physics',
+
+    research: 'Discovery of an exoplanet orbiting a solar-type star',
+
+    year: 2019,
+
+  },
+
+  {
+
+    name: 'John B. Goodenough',
+
+    category: 'Chemistry',
+
+    research: 'Development of lithium-ion batteries',
+
+    year: 2019,
+
+  },
+
+  {
+
+    name: 'M. Stanley Whittingham',
+
+    category: 'Chemistry',
+
+    research: 'Development of lithium-ion batteries',
+
+    year: 2019,
+
+  },
+
+  {
+
+    name: 'Akira Yoshino',
+
+    category: 'Chemistry',
+
+    research: 'Development of lithium-ion batteries',
+
+    year: 2019,
+
+  },
+
+  {
+
+    name: 'Arthur Ashkin',
+
+    category: 'Physics',
+
+    research: 'Optical tweezers and their application to biological systems',
+
+    year: 2018,
+
+  },
+
+  {
+
+    name: 'Gerard Mourou',
+
+    category: 'Physics',
+
+    research: 'Method of generating high-intensity, ultra-short optical pulses',
+
+    year: 2018,
+
+  },
+
+  {
+
+    name: 'Donna Strickland',
+
+    category: 'Physics',
+
+    research: 'Method of generating high-intensity, ultra-short optical pulses',
+
+    year: 2018,
+
+  },
+
+  {
+
+    name: 'Frances H. Arnold',
+
+    category: 'Chemistry',
+
+    research: 'Directed evolution of enzymes',
+
+    year: 2018,
+
+  },
+
+  {
+
+    name: 'George P. Smith',
+
+    category: 'Chemistry',
+
+    research: 'Phage display of peptides and antibodies.',
+
+    year: 2018,
+
+  },
+
+  {
+
+    name: 'Sir Gregory P. Winter',
+
+    category: 'Chemistry',
+
+    research: 'Phage display of peptides and antibodies.',
+
+    year: 2018,
+
+  },
+
+];
+
+
+
+let obj = {};
+
+let count = 0;
+for (let i = 0; i < Array.length; i++) {
+    if (awards[i].category == awards[i + 1]) {
+        
+      }
+}
+console.log(obj)
